3f183d68708ff1ccc9dcb59a3aa220c6a3098666
Jason Waag <jaw977@gmail.com>
2017-06-24 21:50:02 -0400

    dev

--- htdocs/app.js
+++ htdocs/app.js
@@ -3,21 +3,53 @@ window.addEventListener('load', function () {
 		el: '#app',
 		data: {
 			numberOfCols: 3,
-			allTodos: ['First','Second','Third','Fourth','Fifth'],
+			allTodos: [
+				{desc:'First',  done:false, pri:'C', matches:true,},
+				{desc:'Second', done:false, pri:'C', matches:true,},
+				{desc:'Third',  done:false, pri:'C', matches:true,},
+				{desc:'Fourth', done:false, pri:'C', matches:true,},
+				{desc:'Fifth',  done:false, pri:'C', matches:true,},
+			],
 			addTodoTextbox: '',
+			searchMode: 'Open',
+			searchText: '',
 		},
 		computed: {
+			matchingTodos: function() {
+				return this.allTodos.filter( t => t.matches );
+			},
 			numberOfRows: function() {
 				return Math.ceil(this.allTodos.length / this.numberOfCols);
 			},
 			chunkedTodos: function() {
-				return _.chunk(this.allTodos, this.numberOfCols);
+				return _.chunk(this.matchingTodos, this.numberOfCols);
 			},
 		},
 		methods: {
 			addTodo: function() {
-				this.allTodos.push(this.addTodoTextbox);
+				this.allTodos.push({desc:this.addTodoTextbox, done:false, pri:'C', matches:true,});
 				this.addTodoTextbox = '';
+			},
+			todoDoneClass: function(todo) {
+				return 'glyphicon glyphicon-' + (todo.done ? 'check' : 'unchecked');
+			},
+			priClass: function(todo) {
+				return 'btn btn-xs' + (todo.pri == 'A' ? ' btn-danger' : todo.pri == 'B' ? ' btn-primary' : '');
+			},
+			toggleDone: function(todo) {
+				todo.done = todo.done ? false : Date.now();
+			},
+			clickPri: function(todo) {
+				todo.pri = todo.pri == 'A' ? 'C' : todo.pri == 'B' ? 'A' : 'B';
+			},
+			clickSearchMode: function() {
+				this.searchMode = this.searchMode == 'Open' ? 'Done' : 'Open';
+				this.search();
+			},
+			search: function() {
+				for (todo of this.allTodos) {
+					todo.matches = this.searchMode == 'Open' ? ! todo.done : todo.done;
+				}
 			},
 		},
 	});
